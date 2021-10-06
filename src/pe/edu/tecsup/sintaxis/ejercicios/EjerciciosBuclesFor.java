ed0c1c54acdb3bf2e1ff6eff44e29538b47ea0d2
Jaime <you@example.com>
2021-10-05 21:32:36 -0500

    Update

---
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosBuclesFor.java
@@ -0,0 +1,18 @@
+package pe.edu.tecsup.sintaxis.ejercicios;
+
+public class EjerciciosBuclesFor {
+
+	public static void main(String[] args) {
+
+		// 1.- Imprimir los siguientes numeros
+		//      3,6,15,18,21,24
+		
+		
+		// 2.- Sumar los siguientes numeros
+		//      1,3,5,7,9,11,13
+				
+		
+		
+	}
+
+}
