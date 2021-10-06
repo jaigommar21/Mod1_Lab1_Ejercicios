05fbf4b1594254a24d3f6e9060fc86be2c511f84
Jaime <you@example.com>
2021-10-05 19:24:15 -0500

    Update

---
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosBooleanos.java
@@ -0,0 +1,118 @@
+package pe.edu.tecsup.sintaxis.ejercicios;
+
+public class EjerciciosBooleanos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Declarar los datos de una persona
+		// nombre, apellido materno, apellido paterno
+		// edad, sexo , dni y mostrar los datos por consola
+		/*
+		 * String nombre = "Ilder"; String apellidoPaterno = "Tocto"; String
+		 * apellidoMaterno = "Cruz"; String sexo = "Masculino"; String dni = "71318626";
+		 * int edad = 24; System.out.println("Nombre: "+nombre+"\n"
+		 * +"Apellidos: "+apellidoPaterno+" "+apellidoMaterno +"\n" +"Sexo: "+ sexo +
+		 * "\n" + "DNI: "+ dni + "\n" + "Edad: " + edad);
+		 */
+		/*
+		 * String nombre = "Henry", apellido_paterno = "Gamboa", apellido_materno =
+		 * "Rojas", dni = "71582740"; byte edad = 29; char sexo = 'M';
+		 * 
+		 * System.out.println(nombre + " " + apellido_paterno + " " + apellido_materno +
+		 * " tiene " + edad + " años"); System.out.println("Sexo: " + sexo);
+		 * System.out.println("DNI: " + dni); //
+		 */
+
+		// definicion de constante
+//		final double PI = 3.14;
+//		
+//		
+//		String apellidoMaterno  = "Marin";
+//        
+//        String apellido_paterno = "Gomez";
+//
+//        System.out.println(PI);
+//        
+//        
+
+//		String nombre="Edith", ApePater="Andres", ApeMater="Caceres",
+//				edad="32", sexo="F", dni="43965930";
+//				
+//		
+//				System.out.println(nombre);
+//				System.out.println(ApePater);
+//				System.out.println(ApeMater);
+//				System.out.println(edad);
+//				System.out.println(sexo);
+//				System.out.println(dni);
+
+		/*
+		 * byte Edad = 47; String Nombres= "Javier Jesus",ApellidoP =
+		 * "PACIFICO",ApellidoM = "CAVALIER",DocIde = "32135475"; char Sexo = 'M' ;
+		 * System.out.println("Mis Datos Personales:");
+		 * System.out.println("Nombres .........: "+Nombres);
+		 * System.out.println("Apellidos ...... : " + ApellidoP +" " +ApellidoM);
+		 * System.out.println("Doc. de Identidad: " + DocIde);
+		 * System.out.println("Sexo ............: " + Sexo);
+		 */
+
+		/*
+		 * 
+		 * String nombre; String apellido_paterno; String apellido_materno; String sexo;
+		 * String dni; int edad;
+		 * 
+		 * nombre="Jorge"; apellido_paterno="Oblitas"; apellido_materno="Reynoso";
+		 * sexo="Masculino"; dni="12345678"; edad=36;
+		 * 
+		 * System.out.println("Nombre:"+nombre);
+		 * System.out.println("Apellido Paterno:"+apellido_paterno);
+		 * System.out.println("Apellido Materno:"+apellido_materno);
+		 * System.out.println("Sexo:"+sexo); System.out.println("Dni:"+dni);
+		 * System.out.println("Edad:"+edad);
+		 */
+
+		/*
+		 * String nombre, apellido_paterno, apellido_materno, sexo; int edad = 25; int
+		 * dni = 76853134;
+		 * 
+		 * nombre = "Jose"; apellido_paterno = "Parrilla"; apellido_materno =
+		 * "Espinoza"; sexo = "Masculino";
+		 * 
+		 * System.out.println(nombre + " " + apellido_paterno + " " + apellido_materno
+		 * ); System.out.println("es " + sexo + " tiene  " + edad + " anhos," +
+		 * " con dni " + dni );
+		 */
+
+		String name = "Jorge";
+		String apellido = "Sueng";
+		String materno = "Navarrete";
+		short edad = 50;
+		char sexo = 'M';
+		final double PI = 3.14;
+
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+		System.out.println(name + " " + apellido + " " + materno);
+
+		System.out.println("edad ............: " + edad);
+
+		
+		System.out.println("Sexo ............: " + sexo);
+
+	}
+
+}
