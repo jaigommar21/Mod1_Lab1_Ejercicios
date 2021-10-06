6931182130f93590155d4495a1504be605c0f941
Jaime <you@example.com>
2021-10-05 20:30:08 -0500

    Update

--- src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosCondicionales.java
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosCondicionales.java
@@ -3,41 +3,44 @@ package pe.edu.tecsup.sintaxis.ejercicios;
 public class EjerciciosCondicionales {
 
 	public static void main(String[] args) {
-		
-		
+
 		// 1.- Si tengo mas de 4.15 soles puedo comprar dolares
-		//     Imprimir mensaje :"Puedo comprar dolares"
-		
-		int monto = 1 ;
-		if(monto>4.15) {
+		// Imprimir mensaje :"Puedo comprar dolares"
+
+		int monto = 1;
+		if (monto > 4.15) {
 			System.out.println("Puedo Comprar Dolares");
 		}
-		
-		
-		
-		// 2.- Si tengo mas de 4.15 soles puedo comprar dolares
-		//     caso contrario no puedo compralo
-		//     + : Imprimir mensaje :"Puedo comprar dolares"
-		//     - : Imprimir mensaje :"No Puedo comprar dolares"
 
-		
-		monto = 2 ;
-		
-		if(monto>4.15) {
+		int ahorro = 5;
+		final float TIPO_CAMBIO = 4.15f;
+		if (ahorro >= TIPO_CAMBIO) {
+			System.out.println("Si puedo comprar dolares Mi ahorro es " + ahorro);
+		} else {
+			System.out.println("No puedo comprar dolares Mi ahorro es " + ahorro);
+		}
+
+		// 2.- Si tengo mas de 4.15 soles puedo comprar dolares
+		// caso contrario no puedo compralo
+		// + : Imprimir mensaje :"Puedo comprar dolares"
+		// - : Imprimir mensaje :"No Puedo comprar dolares"
+
+		monto = 2;
+
+		if (monto > 4.15) {
 			System.out.println("Puedo Comprar Dolares");
-		}else {
+		} else {
 			System.out.println("No puedo Comprarlo");
 		}
-		
-		
+
 		double saldo = 4.6;
-		
-		if (saldo > 4.5){
+
+		if (saldo > 4.5) {
 			System.out.println("Puedo comprar dolares");
 		} else {
 			System.out.println("No puedo comprar dolares");
 		}
-		
+
 	}
 
 }
