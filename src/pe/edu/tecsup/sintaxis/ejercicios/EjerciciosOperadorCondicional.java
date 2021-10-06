18e3c6d9d5e61ef28ae0787f1af8ae11fa2e335b
Jaime <you@example.com>
2021-10-05 20:08:17 -0500

    Update

--- src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosOperadorCondicional.java
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosOperadorCondicional.java
@@ -9,6 +9,20 @@ public class EjerciciosOperadorCondicional {
 		//     a la variable entera "c" el valor de la variable "a":
 		//     caso1 : a = 50    , caso2 : a = 100
 		
+		int a= 150;
+		int c= (a>=100) ? a: a;
+		System.out.println(c);
+		
+		c= true ? a: -1;
+		
+		c = (a != 'a') ? a : a;
+		System.out.println(c);
+
+		
+		//int a=100;
+		//int c=0;
+		c = (a>c) ? a : a;
+		System.out.println(c);
 		
 		
 		// 2.- Asignar a la variable c el valor de acuerdo a las siguientes
