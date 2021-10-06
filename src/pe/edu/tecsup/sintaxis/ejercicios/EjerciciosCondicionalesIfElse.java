467bbb5ae456fcdeae6d4d8b80b1b8ce8d546090
Jaime <you@example.com>
2021-10-05 20:37:05 -0500

    Update

---
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosCondicionalesIfElse.java
@@ -0,0 +1,18 @@
+package pe.edu.tecsup.sintaxis.ejercicios;
+
+public class EjerciciosCondicionalesIfElse {
+
+	public static void main(String[] args) {
+
+		// 1.- De acuerdo a la edad de una persona 
+		//     mostrar el siguiente mensaje
+		//    - Si su edad es de 0 a 3 años , mostrar "Estara en casa"
+		//	  - Si su edad es de 4 a 6 años , mostrar "Estara en el nido"
+		//	  - Si su edad es de 7 a 12 años , mostrar "Estara en la primaria"
+		//	  - Si su edad es de 13 a 17 años , mostrar "Estara en la secundaria"
+		//    - Si su edad es mayor a 17 años , mostara "Acabo estudios obligatorios"
+		
+
+	}
+
+}
