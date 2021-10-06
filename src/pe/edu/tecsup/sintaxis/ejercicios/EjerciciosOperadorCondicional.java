bf7bdd9abafc1e6c869ad1ba18cac0ac9be713e2
Jaime <you@example.com>
2021-10-05 20:00:51 -0500

    Update

--- src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosOperadorCondicional.java
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosOperadorCondicional.java
@@ -5,12 +5,16 @@ public class EjerciciosOperadorCondicional {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// 1.- Si importar el valor de a, asignar siempre 
-		//     a la variable entera c el valor de a:
-		//     a = 50    , a = 100
+		// 1.- Sin interesar el valor de la variable "a", asignar siempre 
+		//     a la variable entera "c" el valor de la variable "a":
+		//     caso1 : a = 50    , caso2 : a = 100
 		
 		
 		
+		// 2.- Asignar a la variable c el valor de acuerdo a las siguientes
+		//     condiciones
+		//    - asignar el valor de la variable a , cuando esta sea menor a 25
+		//    - si la variable a es mayor o igual a 25 asignar siempre 25  
 		
 
 	}
