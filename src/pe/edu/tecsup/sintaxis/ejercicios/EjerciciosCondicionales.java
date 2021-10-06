c8bc062c163816f68f8f9e0c791130b6dd0c5373
Jaime <you@example.com>
2021-10-05 20:26:32 -0500

    Update

--- src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosCondicionales.java
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosCondicionales.java
@@ -8,7 +8,10 @@ public class EjerciciosCondicionales {
 		// 1.- Si tengo mas de 4.15 soles puedo comprar dolares
 		//     Imprimir mensaje :"Puedo comprar dolares"
 		
-
+		int monto = 1 ;
+		if(monto>4.15) {
+			System.out.println("Puedo Comprar Dolares");
+		}
 		
 		
 		
@@ -18,7 +21,23 @@ public class EjerciciosCondicionales {
 		//     - : Imprimir mensaje :"No Puedo comprar dolares"
 
 		
-
+		monto = 2 ;
+		
+		if(monto>4.15) {
+			System.out.println("Puedo Comprar Dolares");
+		}else {
+			System.out.println("No puedo Comprarlo");
+		}
+		
+		
+		double saldo = 4.6;
+		
+		if (saldo > 4.5){
+			System.out.println("Puedo comprar dolares");
+		} else {
+			System.out.println("No puedo comprar dolares");
+		}
+		
 	}
 
 }
