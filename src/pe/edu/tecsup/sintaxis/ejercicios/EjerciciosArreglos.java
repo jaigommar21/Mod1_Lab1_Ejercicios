1cac28daa74d62805a12478565d71533b7bc141a
Jaime <you@example.com>
2021-10-07 19:33:41 -0500

    Update

--- src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosArreglos.java
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosArreglos.java
@@ -4,31 +4,33 @@ public class EjerciciosArreglos {
 
 	public static void main(String[] args) {
 
-		char vocales[] = new char[5];
+		char vocales[] = new char[6]; // separar espacio en memoria
 
 		vocales[0] = 'a';
 		vocales[1] = 'e';
 		vocales[2] = 'i';
 		vocales[3] = 'o';
 		vocales[4] = 'u';
+		vocales[5] = '.';
 
 		System.out.println(vocales);
 		System.out.println(vocales[0]);
+		System.out.println(vocales[1]);
 		System.out.println(vocales[1]);
 
 		// Ejericio : imprimir los otros valores
 		// vas a usar un for
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 6; i++) {
 
 			System.out.println(vocales[i]);
 
 		}
 		
 		
-//		for (char i = 0; i <= vocales.length; i++) {
-//            System.out.println(vocales);
-//        }
+		for (byte i = 0; i < vocales.length; i++) {
+            System.out.println(vocales[i]);
+        }
 
 	}
 
