9e1475b27784ddf7a3a8f0f5d51de448148fdc83
Jaime <you@example.com>
2021-10-07 21:00:52 -0500

    Update

--- src/pe/edu/tecsup/poo/ejercicios/atributos/Equipo.java
+++ src/pe/edu/tecsup/poo/ejercicios/atributos/Equipo.java
@@ -3,11 +3,9 @@ package pe.edu.tecsup.poo.ejercicios.atributos;
 public class Equipo {
     // atributos
 
-    
-    
     public String pais;
     public String deporte;
-    public Integer cantIntegrantes;
+    public int cantIntegrantes;
     public String nombre;
     
     
