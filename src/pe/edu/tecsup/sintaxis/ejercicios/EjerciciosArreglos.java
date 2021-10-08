75fb6ccebe6d277a66012c569a24ab2f285485a0
Jaime <you@example.com>
2021-10-07 19:11:07 -0500

    Update

---
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosArreglos.java
@@ -0,0 +1,19 @@
+package pe.edu.tecsup.sintaxis.ejercicios;
+
+public class EjerciciosArreglos {
+
+	public static void main(String[] args) {
+
+		char vocales[] = new char[5];
+		
+		vocales[0] = 'a';
+				
+		System.out.println(vocales[0]);		
+		System.out.println(vocales[1]);	
+		
+		// Ejericio : imprimir los otros valores
+		//            vas a usar un for
+		
+	}
+
+}
