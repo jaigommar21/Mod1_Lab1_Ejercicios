9fedeab3bbe8cf60a6ed970eb4be080d6401cdcc
Jaime <you@example.com>
2021-10-07 19:50:06 -0500

    Update

---
+++ src/pe/edu/tecsup/sintaxis/ejercicios/EjerciciosArreglos2D.java
@@ -0,0 +1,16 @@
+package pe.edu.tecsup.sintaxis.ejercicios;
+
+public class EjerciciosArreglos2D {
+
+	public static void main(String[] args) {
+
+		int notasCursoPorAlumno [][] = 
+			{ 
+				{14, 16, 18,  6},  //  Notas de Alumno 1
+				{10,  8, 12, 15},  //  Notas de Alumno 2
+				{16, 18, 14, 20}   //  Notas de Alumno 3
+			};
+
+	}
+
+}
