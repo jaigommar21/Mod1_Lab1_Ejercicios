e62d61ea6d04ddc481efc8c4f3ca7a4fd2829861
Jaime <you@example.com>
2021-10-12 19:55:24 -0500

    Update

---
+++ src/pe/edu/tecsup/poo/ejercicios/constructor/concepto/Proceso.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.poo.ejercicios.constructor.concepto;
+
+public class Proceso {
+
+    /**
+     * 
+     */
+    public Proceso() {
+    
+    	System.out.println("Inicilizando entorno");
+    }
+    
+}
