355676768937133ba5847ed501f5f7cf1e5fad2c
Jaime <you@example.com>
2021-10-15 20:26:08 -0500

    Update

---
+++ src/pe/edu/tecsup/poo/ejercicios/metodos/OperacionesMatematicasNoEstatico.java
@@ -0,0 +1,29 @@
+package pe.edu.tecsup.poo.ejercicios.metodos;
+
+public class OperacionesMatematicasNoEstatico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1.- Crear un metodo que reste dos numeros enteros
+		//     e imprima en consola el resultados 
+		// 
+
+
+		// 2.- Crear un metodo que mulitplique dos numeros enteros
+		//     y retorne el valor, imprimir el resulado obtenido
+		
+
+ 	}
+	
+	public  void imprimeResta(int a, int b) {
+		int resta = a - b ;
+		System.out.println("La Resta es: " + resta);
+ 	}
+	public  int retornaMultiplicacion(int a, int b) {
+		int multi = a * b ;
+		return multi ;
+	}	
+
+
+}
