4ed0f243c7344998f063b9d29e6bb4f94c8a426c
Jaime <you@example.com>
2021-10-15 19:31:22 -0500

    Update

--- src/pe/edu/tecsup/poo/ejercicios/encapsulamiento/Aplicacion.java
+++ src/pe/edu/tecsup/poo/ejercicios/encapsulamiento/Aplicacion.java
@@ -5,14 +5,14 @@ public class Aplicacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Estudiante estudiante1 = new Estudiante("Roberto", "Garcia", 20);
+		Estudiante estudiante1 = new Estudiante("Roberto", "Garcia", 20,"M");
 		
 		System.out.println("Mi nombre es: " + estudiante1.getNombre());
 		System.out.println("Mi apellido es: " +estudiante1.getApellidoPaterno() );
 		System.out.println("Mi edad es: " + estudiante1.getEdad());
 		System.out.println("Mi sexo es: " + estudiante1.getSexo());
 		
-		Estudiante estudiante2 = new Estudiante("Marisol", "Quispe", 23);
+		Estudiante estudiante2 = new Estudiante("Marisol", "Quispe", 23,"F");
 		
 		System.out.println("Mi nombre es: " + estudiante2.getNombre());
 		System.out.println("Mi apellido es: " +estudiante2.getApellidoPaterno() );
--- src/pe/edu/tecsup/poo/ejercicios/encapsulamiento/Estudiante.java
+++ src/pe/edu/tecsup/poo/ejercicios/encapsulamiento/Estudiante.java
@@ -13,6 +13,18 @@ public class Estudiante {
 		this.edad = edad;
 	}
 
+	public Estudiante(String apellidoPaterno, String nombre, int edad, String sexo) {
+		//super();
+		this.apellidoPaterno = apellidoPaterno;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+	}
+
+	
+	// GET : Solo lectura
+	
+	
 	// METODOS :   [AMBITO] [TIPO_DE_DATO_DEVUELVE] [NOMBRE_DEL_METODO] () {
     //              
 	//              }
