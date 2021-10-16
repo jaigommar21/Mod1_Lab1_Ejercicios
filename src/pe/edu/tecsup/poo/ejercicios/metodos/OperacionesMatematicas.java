c90a4dd51f7f175969edd6bed5ae4d6f1f3dd4e9
Jaime <you@example.com>
2021-10-15 20:18:48 -0500

    Update

--- src/pe/edu/tecsup/poo/ejercicios/metodos/OperacionesMatematicas.java
+++ src/pe/edu/tecsup/poo/ejercicios/metodos/OperacionesMatematicas.java
@@ -5,13 +5,29 @@ public class OperacionesMatematicas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int num1 = 20;
+		int num2 = 12;
 		// 1.- Crear un metodo que reste dos numeros enteros
 		//     e imprima en consola el resultados 
 		// 
 		
+		imprimeResta(num1,num2);
+
 		// 2.- Crear un metodo que mulitplique dos numeros enteros
 		//     y retorne el valor, imprimir el resulado obtenido
+		
+		int multi = multiplicaNros(num1,num2);
+		System.out.println("La multiplicacion es " + multi);
+ 	}
 	
-	}
+	public static void imprimeResta(int a, int b) {
+		int resta = a - b ;
+		System.out.println("La Resta es: " + resta);
+ 	}
+	public static int multiplicaNros(int a, int b) {
+		int multi = a * b ;
+		return multi ;
+	}	
+
 
 }
