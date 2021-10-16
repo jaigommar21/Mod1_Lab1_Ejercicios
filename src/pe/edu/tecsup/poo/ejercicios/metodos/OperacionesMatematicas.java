98df8bc9627b137fb8a7bcd2f6365dffbbde687b
Jaime <you@example.com>
2021-10-15 19:57:29 -0500

    Update

---
+++ src/pe/edu/tecsup/poo/ejercicios/metodos/OperacionesMatematicas.java
@@ -0,0 +1,17 @@
+package pe.edu.tecsup.poo.ejercicios.metodos;
+
+public class OperacionesMatematicas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1.- Crear un metodo que reste dos numeros enteros
+		//     e imprima en consola el resultados 
+		// 
+		
+		// 2.- Crear un metodo que mulitplique dos numeros enteros
+		//     y retorne el valor, imprimir el resulado obtenido
+	
+	}
+
+}
