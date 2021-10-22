1adfbd41167d4071ad758a86a9ef59c12d4f61de
Jaime <you@example.com>
2021-10-21 21:50:26 -0500

    Update

--- src/pe/edu/tecsup/collections/list/Auto.java
+++ src/pe/edu/tecsup/collections/list/Auto.java
@@ -1,5 +1,53 @@
 package pe.edu.tecsup.collections.list;
 
+
 public class Auto {
 
+	private String modelo;
+
+	private String nombreMarca;
+
+
+	public Auto(String modelo, String nombreMarca) {
+		super();
+		this.modelo = modelo;
+		this.nombreMarca = nombreMarca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public String getNombreMarca() {
+		return nombreMarca;
+	}
+	
+	@Override
+	public String toString() {
+		return "Auto [modelo=" + modelo + ", nombreMarca=" + nombreMarca + "]";
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (obj instanceof Auto) {
+		
+			Auto auto = (Auto) obj;
+			
+			if (auto.modelo != null && 
+				auto.modelo.equals(this.modelo) && 
+				auto.nombreMarca != null && 
+				auto.nombreMarca.equals(this.nombreMarca)) {
+				
+				return true;
+			
+			}
+		}
+		
+		return false;
+	}
+
+	
+	
 }
--- src/pe/edu/tecsup/collections/list/TestMain5.java
+++ src/pe/edu/tecsup/collections/list/TestMain5.java
@@ -8,6 +8,20 @@ public class TestMain5 {
 	public static void main(String[] args) {
 		
 		List<Auto> autos = new ArrayList<Auto>();
+		
+		autos.add(new Auto("X001", "Toyota"));
+		autos.add(new Auto("X002", "Nissan"));
+		autos.add(new Auto("X003", "Pegaut"));
+
+		
+		Auto autoBuscar = new Auto("X002", "Nissan");
+		
+		if (autos.contains(autoBuscar)) {
+			System.out.println("Auto encontrado");
+		} else {
+			System.out.println("Auto NO encontrado");
+		}
+		
 	}
 
 }
