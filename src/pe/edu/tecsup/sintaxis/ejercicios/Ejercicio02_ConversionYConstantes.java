22b3b4a3e6910fad406f9b476fa36bb5b204d94b
jgomez <jgomez@host.docker.internal>
2022-02-05 11:27:39 -0500

    Update

--- src/pe/edu/tecsup/sintaxis/ejercicios/Ejercicio02_ConversionYConstantes.java
+++ src/pe/edu/tecsup/sintaxis/ejercicios/Ejercicio02_ConversionYConstantes.java
@@ -1,10 +1,22 @@
 package pe.edu.tecsup.sintaxis.ejercicios;
 
 public class Ejercicio02_ConversionYConstantes {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+    public static void main(String[] args) {
+        
+    	byte fecha = 5;
+        int fechaHoy = (int)fecha; // cast es opcional
+        
+        int salario = 50000;
+        byte salarioActual = (byte)salario; // cast es obligatorio pero peligroso
+        
+        final float IGV = 0.18f;
+        final String NOMBRE_PAIS = "PERU";
+        
+        System.out.println(fecha + "\n" +
+                fechaHoy + "\n" + 
+                IGV + "\n" +
+                NOMBRE_PAIS + "\n" +
+                salarioActual);
+    }
 }
