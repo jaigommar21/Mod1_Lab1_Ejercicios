23885f3b23a17eacf0c23e4d6e0312d5d753e159
jgomez <jgomez@host.docker.internal>
2022-02-05 12:18:13 -0500

    Update

---
+++ src/pe/edu/tecsup/sintaxis/ejercicios/Ejercicio03_OperadorCondicional.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.sintaxis.ejercicios;
+
+public class Ejercicio03_OperadorCondicional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x = 5; // otros valores : 25, 50, 100       
+        x = (x < 25) ? 25 : x;
+        
+        System.out.println("Ejemplo: " + x);
+	}
+
+}
