3a6a11b902fc854ef7396328bb2ddf75737b507a
jgomez <jgomez@host.docker.internal>
2022-02-19 09:09:49 -0500

    Update

---
+++ src/pe/edu/tecsup/poo/herencia/caso4/Alumno.java
@@ -0,0 +1,17 @@
+package pe.edu.tecsup.poo.herencia.caso4;
+
+public class Alumno {
+
+	private String nombre;
+	private int edad;
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+}
---
+++ src/pe/edu/tecsup/poo/herencia/caso4/Aplicacion.java
@@ -0,0 +1,20 @@
+package pe.edu.tecsup.poo.herencia.caso4;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		
+		// instanciar la clase
+		Alumno alumno = new Alumno();
+		
+		// Deseo agregar el nombre y la edad
+		// ¿ Que altenativa tengo?
+
+		alumno.setNombre("Jose");
+		alumno.setEdad(26);
+		
+		
+		
+	}
+
+}
