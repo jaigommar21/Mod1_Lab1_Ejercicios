c867e25abb70d007a7bc3e12afc6f59ae56010eb
jgomez <jgomez@host.docker.internal>
2022-02-19 08:36:56 -0500

    Update

---
+++ src/pe/edu/tecsup/poo/ejercicios/_05_polimorfismo/Aplicacion.java
@@ -0,0 +1,12 @@
+package pe.edu.tecsup.poo.ejercicios._05_polimorfismo;
+
+public class Aplicacion {
+	    public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+	        Proceso p1 = new Proceso();
+	        p1.metodo("Hola");
+	        p1.metodo(2);
+	        p1.metodo();
+	                
+	    }
+}
---
+++ src/pe/edu/tecsup/poo/ejercicios/_05_polimorfismo/Proceso.java
@@ -0,0 +1,15 @@
+package pe.edu.tecsup.poo.ejercicios._05_polimorfismo;
+
+public class Proceso {
+    public void metodo() {
+        System.out.println("El proceso se va a ejecutar");
+    }
+    
+    public void metodo(String cadena) {
+        System.out.println("El proceso se está deteniendo");
+    }
+    
+    public void metodo(int numero) {
+        System.out.println("Se esta configurando el proceso");
+    }
+}
