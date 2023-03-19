ac18fb70833bd89883bea6fa63420341f01ff831
jgomezm-tecsup <developer@developers-MacBook-Pro.local>
2023-03-19 12:30:40 -0500

    Update

--- src/pe/edu/tecsup/mod04/_04/lambda/ejercicio/Aplicacion.java
+++ src/pe/edu/tecsup/mod04/_04/lambda/ejercicio/Aplicacion.java
@@ -16,9 +16,11 @@ public class Aplicacion {
             BigDecimal _op1 = new BigDecimal(op1);
             BigDecimal _op2 = new BigDecimal(op2);
            
-            System.out.println(_op1.add(_op2));
+            BigDecimal s = _op1.add(_op2);
             
-            return _op1.add(_op2).doubleValue();
+            System.out.println(s);
+            
+            return s.doubleValue();
         };
         
         System.out.println(om.procesar(1.2, 1.4));
---
+++ src/pe/edu/tecsup/mod04/_06/collections/list/sort/ejercicio/ejercicio-06-04.md
@@ -0,0 +1,4 @@
+
+Dada la clase MarcaProducto , que se usara para almacenar el nombre de una marca que puede ser "HP" ,"Lenovo" , "Samsung"
+  
+Crear un listado denominado nombresMarcas y ordenarlo alfabeticamente por el nombre
