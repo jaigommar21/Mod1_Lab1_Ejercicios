5d3e10de23a79d3c1ad60652f719730e3b152fc1
jgomezm-tecsup <developer@developers-MacBook-Pro.local>
2023-03-19 11:43:29 -0500

    Update

---
+++ src/pe/edu/tecsup/mod04/_04/lambda/ejercicio/Aplicacion.java
@@ -0,0 +1,35 @@
+package pe.edu.tecsup.mod04._04.lambda.ejercicio;
+
+import java.math.BigDecimal;
+
+interface OperacionMatematica {
+
+    double procesar(double op1, double op2);
+}
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+        // Lambda expresion
+        OperacionMatematica om = (op1, op2) -> {
+            BigDecimal _op1 = new BigDecimal(op1);
+            BigDecimal _op2 = new BigDecimal(op2);
+            
+            return _op1.add(_op2).doubleValue();
+        };
+        
+        System.out.println(om.procesar(1.2, 1.4));
+
+        om = (op1, op2) -> {
+            BigDecimal _op1 = new BigDecimal(op1);
+            BigDecimal _op2 = new BigDecimal(op2);
+           
+            return _op1.subtract(_op2).doubleValue();
+        };
+        
+        System.out.println(om.procesar(1.2, 1.4));
+
+        
+    }
+}
