5b545d29140f4ca9239e5cc1eeb1da66d1d1e9fa
jgomezm-tecsup <developer@developers-MacBook-Pro.local>
2023-03-19 09:36:40 -0500

    Update

--- src/pe/edu/tecsup/mod03/_11/util/math/Ejercicio.java
+++ src/pe/edu/tecsup/mod03/_11/util/math/Ejercicio.java
@@ -1,8 +1,8 @@
 package pe.edu.tecsup.mod03._11.util.math;
 
 /**
- * Ejercicio : Crear 5 numeros aleatorios entre 0 y 50 
- *      
+ * Ejercicio : Crear 5 numeros aleatorios entre 0 y 50
+ * 
  * @author developer
  *
  */
@@ -12,21 +12,21 @@ public class Ejercicio {
      * 
      * @param args
      */
-	public static void main(String[] args) {
-		
-		// random
-		for(int i=0; i<5; i++) {
-		    
+    public static void main(String[] args) {
+
+        // random
+        for (int i = 0; i < 5; i++) {
+
             long valor = Math.round(Math.random() * 50) + 3;
             double superior = Math.ceil(valor);
             double potencia = Math.pow(superior, 2);
-            
-            System.out.println("Valor: " + valor);
-			System.out.println("Entero superior: " + superior );
-	        System.out.println("Potencia 2: " + potencia);
-	        
-		}
 
-	}
+            System.out.println("Valor: " + valor);
+            System.out.println("Entero superior: " + superior);
+            System.out.println("Potencia 2: " + potencia);
+
+        }
+
+    }
 
 }
