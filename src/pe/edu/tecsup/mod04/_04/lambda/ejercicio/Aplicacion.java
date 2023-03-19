e75594a865c84a32e364e5dfdf9ff1c4febd37a6
jgomezm-tecsup <developer@developers-MacBook-Pro.local>
2023-03-19 14:26:01 -0500

    Update

--- src/pe/edu/tecsup/mod04/_04/lambda/ejercicio/Aplicacion.java
+++ src/pe/edu/tecsup/mod04/_04/lambda/ejercicio/Aplicacion.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 interface OperacionMatematica {
 
-    double procesar(double op1, double op2);
+    double procesar(BigDecimal op1, BigDecimal op2);
 }
 
 public class Aplicacion {
@@ -13,28 +13,28 @@ public class Aplicacion {
 
         // Lambda expresion
         OperacionMatematica om = (op1, op2) -> {
-            BigDecimal _op1 = new BigDecimal(op1);
-            BigDecimal _op2 = new BigDecimal(op2);
            
-            BigDecimal s = _op1.add(_op2);
+            BigDecimal s = op1.add(op2);
             
             System.out.println(s);
             
             return s.doubleValue();
         };
         
-        System.out.println(om.procesar(1.2, 1.4));
+        System.out.println(om.procesar(
+                new BigDecimal("1.2"), 
+                new BigDecimal("1.4")));
 
         om = (op1, op2) -> {
-            BigDecimal _op1 = new BigDecimal(op1);
-            BigDecimal _op2 = new BigDecimal(op2);
            
-            System.out.println(_op1.subtract(_op2));
+            System.out.println(op1.subtract(op2));
             
-            return _op1.subtract(_op2).doubleValue();
+            return op1.subtract(op2).doubleValue();
         };
         
-        System.out.println(om.procesar(1.2, 1.4));
+        System.out.println(om.procesar(
+                new BigDecimal("1.2"), 
+                new BigDecimal("1.4")));
 
         
     }
